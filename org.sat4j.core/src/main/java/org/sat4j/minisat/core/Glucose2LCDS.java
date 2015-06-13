2e9d658e9d69cf6b0afa84957a8d16cf5dd43450
Eugene Yokota <eed3si9n@gmail.com>
2015-06-13 16:28:15 -0400

    Adds sat4j Rev 2404 from svn://svn.forge.objectweb.org/svnroot/sat4j

---
+++ org.sat4j.core/src/main/java/org/sat4j/minisat/core/Glucose2LCDS.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004, 2012 Artois University and CNRS
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ *
+ * Based on the original MiniSat specification from:
+ *
+ * An extensible SAT solver. Niklas Een and Niklas Sorensson. Proceedings of the
+ * Sixth International Conference on Theory and Applications of Satisfiability
+ * Testing, LNCS 2919, pp 502-518, 2003.
+ *
+ * See www.minisat.se for the original solver in C++.
+ *
+ * Contributors:
+ *   CRIL - initial API and implementation
+ *******************************************************************************/
+package org.sat4j.minisat.core;
+
+import org.sat4j.specs.Constr;
+
+class Glucose2LCDS<D extends DataStructureFactory> extends GlucoseLCDS<D> {
+
+    /**
+     * 
+     */
+    private final Solver<D> solver;
+    /**
+    	 * 
+    	 */
+    private static final long serialVersionUID = 1L;
+
+    Glucose2LCDS(Solver<D> solver, ConflictTimer timer) {
+        super(solver, timer);
+        this.solver = solver;
+    }
+
+    @Override
+    public String toString() {
+        return "Glucose 2 learned constraints deletion strategy (LBD updated on propagation) with timer "
+                + getTimer();
+    }
+
+    @Override
+    public void onPropagation(Constr from) {
+        if (from.getActivity() > 2.0) {
+            int nblevel = computeLBD(from);
+            if (nblevel < from.getActivity()) {
+                solver.stats.updateLBD++;
+                from.setActivity(nblevel);
+            }
+        }
+    }
+
+}
